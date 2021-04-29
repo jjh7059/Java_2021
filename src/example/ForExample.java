4856f7591da93c782d8c62913d407359931145bd
STU <STU@DESKTOP-0JFOU0C>
2021-04-29 10:23:22 +0900

    5day problems complete

--- src/example/ForExample.java
+++ src/example/ForExample.java
@@ -7,35 +7,68 @@ public class ForExample {
 		for(int i = 1; i <= 7; i++) {
 			System.out.print("진주현 ");
 		}
+		System.out.println();
 		System.out.println("========================================");
+		
 		
 		//1~10 범위의 정수 중 홀수를 화면에 출력하세요.
 		//ex) 1  3  5  7  9
+		for(int i = 1; i <= 9; i++) {
+			if(i % 2 == 1) {
+				System.out.print(i + "\t");
+			}
+		}
+		System.out.println();
 		System.out.println("========================================");
+		
 		
 		//1~100 범위의 정수에서 3의 배수들의 합계와 5의 배수들의 합계를 계산하여
 		//3의 배수의 합계에서 5의 배수의 합계를 뺀 결과를 출력하세요.
 		//ex) 결과 = 633
+		int sum_3 = 0, sum_5 = 0;
 		for(int i = 1; i <= 100; i++) {
-			
+			if(i % 3 == 0) {
+				sum_3 += i;
+			} else if (i % 5 == 0) {
+				sum_5 += i;
+			}
 		}
+		System.out.println("1~100 범위에서 3의 배수 합계 - 5의 배수 합계 = " + (sum_3 - sum_5));
 		System.out.println("========================================");
+		
 		
 		//구구단 중 7단을 화면에 출력하세요.
 		//ex) 7 * 1 = 7
 		//    7 * 2 = 14
 		//      ...
 		//    7 * 9 = 63
+		for(int i = 1; i <= 9; i++) {
+			System.out.println("7 * " + i + " = " + 7 * i);
+		}
 		System.out.println("========================================");
+		
 		
 		//5!의 결과값을 계산하여 결과값을 출력하세요.(5! = 5 * 4 * 3 * 2 * 1)
 		//ex)5! = 120
+		int a = 1;
+		for(int i = 5; i >= 1; i--) {
+			a *= i;
+		}
+		System.out.println("5! = " + a);
 		System.out.println("========================================");
+		
 		
 		//두 변수에 저장된 정수값 사이의 정수들을 화면에 출력하세요.
 		//단, 한 줄에 정수값이 7개씩 출력되도록 작성하세요.
 		//ex) 36 27 38 39 40 41 42
 		int begin = 36, end = 57;
+		for(int i = begin-1; i <= end; i++) {
+			for(int j = 1; j <= 7; j++) {
+				System.out.print(i+j + " ");				
+			}
+			System.out.println();
+		}
+		System.out.println();
 		System.out.println("========================================");
 	}
 }
--- src/example/MultiForExample.java
+++ src/example/MultiForExample.java
@@ -7,23 +7,65 @@ public class MultiForExample {
 		//¡Ú¡Ú¡Ú
 		//¡Ú¡Ú
 		//¡Ú
+		for(int i = 1; i <= 5; i++) {
+			for(int j = 5; j >= i; j--) {
+				System.out.print("¡Ú");
+			}
+			System.out.println();
+		}
+		System.out.println("========================================");
 		
-		//¡Ú¡Ú¡Ú¡Ú¡Ú
-		//¡Ù¡Ú¡Ú¡Ú¡Ú
-		//¡Ù¡Ù¡Ú¡Ú¡Ú
-		//¡Ù¡Ù¡Ù¡Ú¡Ú
+		
+		//¡Ú¡Ú¡Ú¡Ú¡Ú i = 1, Èò0 °Ë5
+		//¡Ù¡Ú¡Ú¡Ú¡Ú i = 2, Èò1 °Ë4
+		//¡Ù¡Ù¡Ú¡Ú¡Ú i = 3, Èò2 °Ë3
+		//¡Ù¡Ù¡Ù¡Ú¡Ú 
 		//¡Ù¡Ù¡Ù¡Ù¡Ú
+		for(int i = 1; i <= 5; i++) {
+			for(int j = 5; j >= i; j--) {
+				System.out.print("¡Ú");
+			}
+			for(int j = 1; j <= i; j++) {
+				System.out.print("¡Ù");
+			}
+			System.out.println();
+		}
+		System.out.println("========================================");
+		
+		
 		
 		//¡Ú
 		//¡Ú¡Ú¡Ú
 		//¡Ú¡Ú¡Ú¡Ú¡Ú
 		//¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú
 		//¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú¡Ú
+		for(int i = 1; i <= 9; i++) {
+			for(int j =1; j <= i; j++) {
+				if(i % 2 == 1) {
+					System.out.print("¡Ú");					
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("========================================");
+		
 		
 		//¡Ú¡Ù¡Ù¡Ù¡Ú
 		//¡Ù¡Ú¡Ù¡Ú¡Ù
 		//¡Ù¡Ù¡Ú¡Ù¡Ù
 		//¡Ù¡Ú¡Ù¡Ú¡Ù
 		//¡Ú¡Ù¡Ù¡Ù¡Ú
+		for(int i = 1; i <= 5; i++) {
+			for(int j = 1; j <= 5; j++) {
+				if(i == j || i == 1 && j == 5 || i == 5 && j == 1 || 
+						i == 2 && j == 4 || i == 4 && j == 2) {
+					System.out.print("¡Ú");
+				} else {
+					System.out.print("¡Ù");					
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("========================================");
 	}
 }
