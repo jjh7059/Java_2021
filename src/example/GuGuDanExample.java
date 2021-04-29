39ceebb5282156fe2c79839f64db50717bbf5e09
STU <STU@DESKTOP-0JFOU0C>
2021-04-29 18:06:30 +0900

    totalone solved

---
+++ src/example/GuGuDanExample.java
@@ -0,0 +1,14 @@
+package example;
+
+//키보드로 정수값을 입력받아 1~9 범위의 정수값을 곱한 결과를 출력하는 프로그램을 작성하세요
+//단, 입력된 정수값은 2~9 범위의 정수값만 허용되며 범위 밖의 정수값을 입력한 경우
+//메시지 출력후 재입력
+//ex) 단 입력[2~9] >> 7
+//    7 * 1 = 7
+//       ...
+//    7 * 9 = 63
+public class GuGuDanExample {
+	public static void main(String[] args) {
+		
+	}
+}
